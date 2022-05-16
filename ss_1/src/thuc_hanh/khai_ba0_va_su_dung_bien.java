2aefdacf770368cf4b9d577f2aa03b72434c7ff2
Phamtheduyet123 <duyetpham322@gmail.com>
2022-05-16 10:00:59 +0700

    nop bai

---
+++ ss_1/src/thuc_hanh/khai_ba0_va_su_dung_bien.java
@@ -0,0 +1,13 @@
+package thuc_hanh;
+
+public class khai_ba0_va_su_dung_bien {
+    public static void main(String[] args) {
+        int i =10;
+        float f =20.5f;
+        double d =20.5;
+        boolean b = true;
+        char c ='a';
+        String s ="Ha noi";
+        System.out.println(i+" "+f+" "+" "+ d+" "+ b+" "+c+" "+s);
+    }
+}
