25657b77dd27b3bf2a1c914ad077aa370dbb627e
Phamtheduyet123 <duyetpham322@gmail.com>
2022-05-16 13:21:51 +0700

    nam nhuan

---
+++ ss_1/src/thuc_hanh/nam_nhuan.java
@@ -0,0 +1,25 @@
+package thuc_hanh;
+
+import java.util.Scanner;
+
+public class nam_nhuan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year:");
+        int year = sc.nextInt();
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println(year+"is a leap year");
+                }
+                else
+                    System.out.println("is not a leap year");
+            }
+            else
+                System.out.println(year+"is a leap year");
+        }
+        else {
+            System.out.println("is not a leap year");
+        }
+    }
+}
