bd131dca995196997e908eab7d0e717a605dd0e4
Phamtheduyet123 <duyetpham322@gmail.com>
2022-06-15 19:23:45 +0700

    nop bai

--- ss_11/src/bai_tap/treemap/CountChar.java
+++ ss_11/src/bai_tap/treemap/CountChar.java
@@ -2,22 +2,22 @@ package bai_tap.treemap;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class CountChar {
     public static void main(String[] args) {
         String stTest = "Hello World";
         TreeMap<Character, Integer> treeMap = new TreeMap<>();
-        Arrays.asList(stTest.toUpperCase().split("")).forEach(e -> {
+        String[] arr = stTest.split("");
+        for (int i = 0; i < arr.length; i++) {
             int count = 1;
-            if (treeMap.containsKey(e.charAt(0))) {
-                count += treeMap.get(e.charAt(0));
+            if (treeMap.containsKey(arr[i].charAt(0))) {
+                count += treeMap.get(arr[i].charAt(0));
             }
-//            if(!e.isBlank())
-            treeMap.put(e.charAt(0), count);    
-        });
-
-        for (Map.Entry<Character, Integer> s : treeMap.entrySet()) {
+            treeMap.put(arr[i].charAt(0), count);
+        }
+        for (Map.Entry<Character, Integer> s: treeMap.entrySet()) {
             System.out.println(s);
         }
     }
